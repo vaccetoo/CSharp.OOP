667a898cfd592838c24fd6e7b1e32baffef78c7f
Vasil Delchev <vasildelchev97@gmail.com>
2024-02-27 21:48:16 +0200

    Added exercieses

---
+++ 03.InterfacesAndAbstraction-Exercise/PersonInfo/Citizen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthday)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthday;
+        }
+
+
+
+        public string Name
+        {
+            get; private set;
+        }
+
+
+        public int Age
+        {
+            get; private set;
+        }
+
+
+        public string Id
+        {
+            get; private set;
+        }
+
+
+        public string Birthdate
+        {
+            get; private set;
+        }
+    }
+}
