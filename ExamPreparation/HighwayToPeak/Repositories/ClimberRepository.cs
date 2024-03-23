f09c6157045786f4a142631283888594515c237d
Vasil Delchev <vasildelchev97@gmail.com>
2024-03-23 20:23:21 +0200

    local commit

---
+++ ExamPreparation/HighwayToPeak/Repositories/ClimberRepository.cs
@@ -0,0 +1,31 @@
+﻿using HighwayToPeak.Models.Contracts;
+using HighwayToPeak.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayToPeak.Repositories
+{
+    public class ClimberRepository : IRepository<IClimber>
+    {
+        private readonly List<IClimber> climbers;
+
+        public ClimberRepository()
+        {
+            climbers = new List<IClimber>();
+        }
+
+        public IReadOnlyCollection<IClimber> All 
+            => climbers;
+
+        public void Add(IClimber model)
+        {
+            climbers.Add(model);
+        }
+
+        public IClimber Get(string name)
+            => climbers.FirstOrDefault(c => c.Name == name);
+    }
+}
