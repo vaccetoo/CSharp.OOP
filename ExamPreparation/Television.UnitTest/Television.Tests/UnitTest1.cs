b581faf7505b50628e40bd65280b60a37008ba8a
Vasil Delchev <vasildelchev97@gmail.com>
2024-03-23 09:52:53 +0200

    Added Tests fot TelevisionDevice

--- ExamPreparation/Television.UnitTest/Television.Tests/UnitTest1.cs
+++ ExamPreparation/Television.UnitTest/Television.Tests/UnitTest1.cs
@@ -1,21 +1,138 @@
 namespace Television.Tests
 {
     using System;
+    using System.Diagnostics;
+    using System.Net.Http.Headers;
     using NUnit.Framework;
     public class Tests
     {
-        [TestCase("tv", 1.1, 1, 2)]
-        public void TestConstructorSavesCorrectlyData(string brand, double price, int screenWidth, int screenHeigth)
+        private readonly TelevisionDevice tv = new ("TV", 1.1, 1, 1);
+       
+        [Test]
+        public void TestingConstructor()
         {
-            TelevisionDevice tv = new(brand, price, screenWidth, screenHeigth);
+            string expectedBrand = "TV";
+            double expectedPrice = 1.1;
+            int expectedWidth = 1;
+            int expectedHeigth = 1;
 
-            Assert.AreEqual(brand, tv.Brand);
-            Assert.AreEqual(price, tv.Price);
-            Assert.AreEqual(screenWidth, tv.ScreenWidth);
-            Assert.AreEqual(screenHeigth, tv.ScreenHeigth);
+            Assert.AreEqual(expectedBrand, tv.Brand);
+            Assert.AreEqual(expectedPrice, tv.Price);
+            Assert.AreEqual(expectedWidth, tv.ScreenWidth);
+            Assert.AreEqual(expectedHeigth, tv.ScreenHeigth);
         }
 
-        [TestCase()]
-        
+        [Test]
+        public void TestCurrentChannelGetteris0()
+        {
+            int expectedResult = 0;
+            int actualResult = tv.CurrentChannel;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void TestingVolumeGetterIs13()
+        {
+            int expectedResult = 13;
+            int actualResult = tv.Volume;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void CheckIsTVMutedShouldReturnFalse()
+        {
+            bool expectedResult = false;
+            bool actualResult = tv.IsMuted;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void SwitchOnShouldReturnCorrectString()
+        {
+            string expectedResult = "Cahnnel 0 - Volume 13 - Sound On";
+            string actualResult = tv.SwitchOn();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(22)]
+        [TestCase(1)]
+        [TestCase(111)]
+        public void ChangeChannelReturnCorrectResult(int newChanel)
+        {
+            TelevisionDevice television = new("TV2", 1.1, 1, 1);
+            
+            int expectedResult = newChanel;
+            int actualResult = television.ChangeChannel(newChanel);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(-1)]
+        [TestCase(-333)]
+        public void ChangeChanelThrowsExceptionIfChanelLessThan0(int newChanel)
+        {
+            TelevisionDevice television = new("TV2", 1.1, 1, 1);
+
+            Assert.Throws<ArgumentException>(() => television.ChangeChannel(newChanel), "Invalid key!");
+        }
+
+        [TestCase(88)]
+        [TestCase(89)]
+        public void VolumeChangeReturnCorrectStringIfUpMoreThan100(int units)
+        {
+            TelevisionDevice television = new("TV2", 1.1, 1, 1);
+
+            string expectedResult = "Volume: 100";
+            string actualResult = television.VolumeChange("UP", units);
+
+            Assert.AreEqual(expectedResult, actualResult);  
+        }
+
+        [TestCase(14)]
+        [TestCase(-15)]
+        public void VolumeChangeReturnCorrectStringIfLessThan0(int units)
+        {
+            TelevisionDevice television = new("TV2", 1.1, 1, 1);
+
+            string expectedResult = "Volume: 0";
+            string actualResult = television.VolumeChange("DOWN", units);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase(10)]
+        public void VolumeChangeReturnCorrectStringIfWorksRegular(int units)
+        {
+            TelevisionDevice television = new("TV2", 1.1, 1, 1);
+
+            string expectedResult = "Volume: 23";
+            string actualResult = television.VolumeChange("UP", units);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void MuteDeviceReturnCorrectBool()
+        {
+            TelevisionDevice television = new("TV2", 1.1, 1, 1);
+
+            bool expectedResult = true;
+            bool actualResult = television.MuteDevice();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void TestToStringOverrrideIsCoorect()
+        {
+            TelevisionDevice television = new("TV2", 1.1, 1, 1);
+
+            Assert.AreEqual($"TV Device: TV2, Screen Resolution: 1x1, Price 1.1$", television.ToString());
+        }
+
     }
 }
